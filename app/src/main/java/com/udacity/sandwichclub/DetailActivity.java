ee695c7714c4832e92960914a9d418f0e12fcaf6
Stefanie Molin <stefaniemolin@gmail.com>
2018-04-28 22:39:41 -0400

    Submit project 1 for review

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,17 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView originTV = (TextView) findViewById(R.id.origin_tv);
+        originTV.setText(sandwich.getPlaceOfOrigin());
 
+        TextView descriptionTV = (TextView) findViewById(R.id.description_tv);
+        descriptionTV.setText(sandwich.getDescription());
+
+        TextView ingredientsTV = (TextView) findViewById(R.id.ingredients_tv);
+        ingredientsTV.setText(JsonUtils.arrayToString(sandwich.getIngredients()));
+
+        TextView akaTV = (TextView) findViewById(R.id.also_known_tv);
+        akaTV.setText(JsonUtils.arrayToString(sandwich.getAlsoKnownAs()));
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,145 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        /** Parse description of a sandwich as JSON and return a Sandwich object without using
+         * any 3rd party libraries.
+         *
+         * @param json String containing the JSON of the information about the sandwich
+         *
+         * @return Sandwich object
+         */
+
+        /* Sample JSON:
+         {\"name\":{\"mainName\":\"Medianoche\",\"alsoKnownAs\":[\"Cuban
+         Sandwich\"]},\"placeOfOrigin\":\"Cuba\",\"description\":\"Medianoche (\\\"midnight\\\"
+         in Spanish) is a type of sandwich which originated in Cuba. It is served in many Cuban
+         communities in the United States. It is so named because of the sandwich\'s popularity
+         asa staple served in Havana\'s night clubs right around or after
+         midnight.\",\"image\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Sandwich_de_Medianoche.jpg/800px-Sandwich_de_Medianoche.jpg\",\"ingredients\":[\"Egg
+         bread\",\"Roast pork\",\"Ham\",\"Mustard\",\"Swiss cheese\",\"Dill pickles\"]}
+
+         Sandwich object:
+         Sandwich(String mainName, List<String> alsoKnownAs, String placeOfOrigin,
+                  String description, String image, List<String> ingredients)
+         */
+        json = json.replace("\"", "").replace("\\", "");
+
+        /* This now looks like (spacing is for ease of understanding only)
+        {
+           name:{
+                 mainName:Medianoche,
+                 alsoKnownAs:[Cuban Sandwich]
+                 },
+           placeOfOrigin:Cuba,
+           description:Medianoche (midnight in Spanish) is a type of sandwich which originated in Cuba.
+                       It is served in many Cuban communities in the United States.
+                       It is so named because of the sandwich's popularity as a staple served in Havana's
+                       night clubs right around or after midnight.,
+           image:https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Sandwich_de_Medianoche.jpg/800px-Sandwich_de_Medianoche.jpg,
+           ingredients:[Egg bread,Roast pork,Ham,Mustard,Swiss cheese,Dill pickles]
+         }
+        */
+
+        String jsonArray[] = json.split("\\{");
+
+        // Parse sandwich name
+        Pattern namePattern = Pattern.compile("(?:(mainName:))(\\w*)");
+        Matcher nameMatcher = namePattern.matcher(jsonArray[2]);
+        String sandwichName;
+        if (nameMatcher.find()) {
+            sandwichName = nameMatcher.group(2);
+        } else {
+            sandwichName = "";
+        }
+
+        // Parse sandwich AKA's
+        Pattern akaPattern = Pattern.compile("(?:(alsoKnownAs:))(([^\\}])*)");
+        Matcher akaMatcher = akaPattern.matcher(jsonArray[2]);
+        String[] akaNames;
+        if (akaMatcher.find()) {
+            akaNames = akaMatcher.group(2).replaceAll("[\\[\\]]", "").split(",");
+        } else {
+            akaNames = new String[1];
+            akaNames[0] = "N/A";
+        }
+        ArrayList<String> akas = new ArrayList<>();
+        for (String aka : akaNames) {
+            if (aka != null && aka != ""){
+                akas.add(aka);
+            } else {
+                akas.add("N/A");
+            }
+        }
+
+        // Parse sandwich origin
+        Pattern originPattern = Pattern.compile("(?:(placeOfOrigin:))(.*)(?=,description)");
+        Matcher originMatcher = originPattern.matcher(jsonArray[2]);
+        String origin;
+        if (originMatcher.find()) {
+            origin = originMatcher.group(2);
+            if (origin == null || origin == "") {
+                origin = "Unknown";
+            }
+        } else {
+            origin = "Unknown";
+        }
+
+        // Parse sandwich description
+        Pattern descriptionPattern = Pattern.compile("(?:(description:))(.*)(?=,image)");
+        Matcher descriptionMatcher = descriptionPattern.matcher(jsonArray[2]);
+        String description;
+        if (descriptionMatcher.find()) {
+            description = descriptionMatcher.group(2);
+        } else {
+            description = "Description missing.";
+        }
+
+        // Parse sandwich image
+        Pattern imagePattern = Pattern.compile("(?:(image:))(.*)(?:,)(?=ingredients)");
+        Matcher imageMatcher = imagePattern.matcher(jsonArray[2]);
+        String image;
+        if (imageMatcher.find()) {
+            image = imageMatcher.group(2);
+        } else {
+            image = "";
+        }
+
+        // Parse sandwich ingredients
+        Pattern ingredientsPattern = Pattern.compile("(?:(ingredients:))(([^\\}])*)");
+        Matcher ingredientsMatcher = ingredientsPattern.matcher(jsonArray[2]);
+        String[] ingredients;
+        if (ingredientsMatcher.find()) {
+            ingredients = ingredientsMatcher.group(2).replaceAll("[\\[\\]]", "").split(",");
+        } else {
+            ingredients = new String[1];
+            ingredients[0] = "Ingredients missing!";
+        }
+        ArrayList<String> ingredientsArrayList = new ArrayList<>();
+        for (String ingredient : ingredients) {
+            ingredientsArrayList.add(ingredient);
+        }
+        return new Sandwich(sandwichName, akas, origin, description, image, ingredientsArrayList);
+    }
+
+    public static String arrayToString(List<String> array){
+        String stringRepresentation = "";
+        for(int i = 0; i < array.size(); i++){
+            stringRepresentation += array.get(i);
+            if(i < array.size() - 1){
+                stringRepresentation += ", ";
+            }
+        }
+        return stringRepresentation;
     }
 }
